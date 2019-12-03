f4e09e20b7269d8797363928d23c3f98e3fac8ad
AshlynL <46731571+AshlynL@users.noreply.github.com>
2019-12-03 09:58:24 -0500

    beta

--- Levinson_finalproj/Levinson_finalproj/main.asm
+++ Levinson_finalproj/Levinson_finalproj/main.asm
@@ -21,12 +21,35 @@ setup:
 	rcall OLED_initialize
 	rcall GFX_clear_array
 	rcall GFX_refresh_screen
+	ldi r16, 0b00000000
+	sts PORTB_DIR, r16
+	ldi r16, 0b00001000
+	sts PORTB_PIN4CTRL, r16
+	ldi r19, 1
+
+	;clc 
+	;rol r16
+	;brcc nj1
+	;ldi r20, 29
+	;eor r16, r20
+	;nj1:
 
 loop:
-	ldi r18, 8
-	ldi r19, 1
+	rcall GFX_clear_array
+	rcall check_button
+	ldi r18, 1
 	rcall GFX_set_array_pos
 	ldi r17, 42
 	st X, r17
 	rcall GFX_refresh_screen
 	rjmp loop
+
+check_button:
+	lds r20, PORTB_IN
+	andi r20, 0b00010000
+	cpi r20, 0b00000000
+	breq increment
+	ret
+increment:
+	inc r19
+	ret
